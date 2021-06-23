52af8423b62b972e3d0f0918b8e9bc95f68ed294
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-06-23 16:23:56 +0700

    Initial Commit

---
+++ CoreData2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData2
+//
+//  Created by Mixko on 23/6/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
